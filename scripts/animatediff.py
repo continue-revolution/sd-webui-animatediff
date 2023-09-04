8f9bedbd544f49302d20133351c9f238e5ecbb9b
Chengsong Zhang <continuerevolution@gmail.com>
2023-09-04 16:17:28 -0500

    any community model supported

--- scripts/animatediff.py
+++ scripts/animatediff.py
@@ -29,6 +29,16 @@ script_dir = scripts.basedir()
 groupnorm32_original_forward = GroupNorm32.forward
 
 
+class ToolButton(gr.Button, gr.components.FormComponent):
+    """Small button with single emoji as text, fits inside gradio forms"""
+
+    def __init__(self, **kwargs):
+        super().__init__(variant="tool", **kwargs)
+
+    def get_block_name(self):
+        return "button"
+
+
 class AnimateDiffScript(scripts.Script):
     motion_module: MotionWrapper = None
 
@@ -56,8 +66,25 @@ class AnimateDiffScript(scripts.Script):
         gc.collect()
 
     def ui(self, is_img2img):
+        model_dir = shared.opts.data.get("animatediff_model_path", os.path.join(script_dir, "model"))
+        if not os.path.isdir(model_dir):
+            os.mkdir(model_dir)
+        model_list = [f for f in os.listdir(model_dir) if f != ".gitkeep"]
         with gr.Accordion('AnimateDiff', open=False):
-            model = gr.Dropdown(choices=["mm_sd_v15.ckpt", "mm_sd_v14.ckpt"], value="mm_sd_v15.ckpt", label="Motion module", type="value")
+            with gr.Row():
+                def refresh_models(*inputs):
+                    new_model_list = [f for f in os.listdir(model_dir) if f != ".gitkeep"]
+                    dd = inputs[0]
+                    if dd in new_model_list:
+                        selected = dd
+                    elif len(new_model_list) > 0:
+                        selected = new_model_list[0]
+                    else:
+                        selected = None
+                    return gr.Dropdown.update(choices=new_model_list, value=selected)
+                model = gr.Dropdown(choices=model_list, value=model_list[0], label="Motion module", type="value")
+                refresh_model = ToolButton(value='\U0001f504')
+                refresh_model.click(refresh_models, model, model)
             with gr.Row():
                 enable = gr.Checkbox(value=False, label='Enable AnimateDiff')
                 video_length = gr.Slider(minimum=1, maximum=24, value=16, step=1, label="Number of frames", precision=0)
@@ -75,16 +102,21 @@ class AnimateDiffScript(scripts.Script):
         if not os.path.isfile(model_path):
             raise RuntimeError("Please download models manually.")
         if AnimateDiffScript.motion_module is None or AnimateDiffScript.motion_module.mm_type != model_name:
-            if shared.opts.data.get("animatediff_check_hash", True):
-                def get_mm_hash(model_name="mm_sd_v15.ckpt"):
-                    if model_name == "mm_sd_v14.ckpt":
-                        return 'aa7fd8a200a89031edd84487e2a757c5315460eca528fa70d4b3885c399bffd5'
-                    elif model_name == "mm_sd_v15.ckpt":
-                        return 'cf16ea656cb16124990c8e2c70a29c793f9841f3a2223073fac8bd89ebd9b69a'
-                    else:
-                        raise RuntimeError(f"Unsupported model filename {model_name}. Should be one of mm_sd_v14.ckpt or mm_sd_v15.ckpt")
-                if hashes.sha256(model_path, f"AnimateDiff/{model_name}") != get_mm_hash(model_name):
-                    raise RuntimeError(f"{model_name} hash mismatch. You probably need to re-download the motion module.")
+            def get_mm_hash(model_name="mm_sd_v15.ckpt"):
+                model_hash = hashes.sha256(model_path, f"AnimateDiff/{model_name}")
+                if model_hash == 'aa7fd8a200a89031edd84487e2a757c5315460eca528fa70d4b3885c399bffd5':
+                    self.logger.info('You are using mm_sd_14.ckpt, which has been tested and supported.')
+                elif model_hash == "cf16ea656cb16124990c8e2c70a29c793f9841f3a2223073fac8bd89ebd9b69a":
+                    self.logger.info('You are using mm_sd_15.ckpt, which has been tested and supported.')
+                elif model_hash == "0aaf157b9c51a0ae07cb5d9ea7c51299f07bddc6f52025e1f9bb81cd763631df":
+                    self.logger.info('You are using mm-Stabilized_high.pth, which has been tested and supported.')
+                elif model_hash == '39de8b71b1c09f10f4602f5d585d82771a60d3cf282ba90215993e06afdfe875':
+                    self.logger.info('You are using mm-Stabilized_mid.pth, which has been tested and supported.')
+                else:
+                    self.logger.warn(f"Your model {model_name} has not been tested and supported. "
+                                     "Either your download is incomplete or your model has not been tested. "
+                                     "Please use at your own risk.")
+            get_mm_hash(model_name)
             self.logger.info(f"Loading motion module {model_name} from {model_path}")
             mm_state_dict = torch.load(model_path, map_location=device)
             AnimateDiffScript.motion_module = MotionWrapper(model_name)
@@ -157,7 +189,6 @@ class AnimateDiffScript(scripts.Script):
 def on_ui_settings():
     section = ('animatediff', "AnimateDiff")
     shared.opts.add_option("animatediff_model_path", shared.OptionInfo(os.path.join(script_dir, "model"), "Path to save AnimateDiff motion modules", gr.Textbox, section=section))
-    shared.opts.add_option("animatediff_check_hash", shared.OptionInfo(True, "Check hash for motion modules. Disable checking if you want to use your own.", gr.Checkbox, section=section))
 
 
 script_callbacks.on_ui_settings(on_ui_settings)
