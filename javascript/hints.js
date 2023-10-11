f6aa3c5fe63bf327e1e1547785ac482cdbe3ace8
Jack Smith <jackdsmith2327@gmail.com>
2023-10-11 00:39:35 -0700

    Added easy to understand tooltips for missing tooltips (#151)
    * Added easy to understand tooltips for missing tooltips
    
    Used an AI to make easy tooltips.
    
    * move tooltip to js, but leave some content to a future pull
    
    ---------
    
    Co-authored-by: Chengsong Zhang <continuerevolution@gmail.com>

---
+++ javascript/hints.js
@@ -0,0 +1,20 @@
+if (titles) { // TODO: This is a conflict feature. If some other extension also have the same textContent, they will also show this hint, or substitite this hint.
+    titles["Motion module"]         = "Choose which motion module to be injected into UNet.";
+    titles["Number of frames"]      = "Total length of video in frames.";
+    titles["FPS"]                   = "How many frames per second generated GIF will be.";
+    titles["Display loop number"]   = "How many times the animation will loop when displayed, a value of 0 will loop forever.";
+    titles["Closed loop"]           = "If enabled, will try to make the last frame the same as the first frame.";
+    titles["Context batch size"]    = "TODO";
+    titles["Stride"]                = "Adjusts the spacing between selected frames in the animation. A larger stride creates more variation between frames.";
+    titles["Overlap"]               = "Number of frames to overlap in context.";
+    titles["Save"]                  = "Which formats the animation should be saved in";
+    titles["Reverse"]               = "Reverse the resulting animation, remove the first and/or last frame from duplication.";
+    titles["Frame Interpolation"]   = "Interpolate between frames with Deforum's FILM implementation. Requires Deforum extension.";
+    titles["Interp X"]              = "Replace each input frame with X interpolated output frames.";
+    titles["Video source"]          = "Paste path to video file.";
+    titles["Video path"]            = "TODO";
+    titles["Latent power"]          = "TODO";
+    titles["Latent scale"]          = "TODO";
+    titles["Optional latent power for last frame"] = "TODO";
+    titles["Optional latent scale for last frame"] = "TODO";
+}
